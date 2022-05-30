0090ea13e8ce408cd3ea7e144b2b878c81bc2f81
Matthieu Riegler <kyro38@gmail.com>
2022-05-30 03:43:58 +0200

    useNavigate to update URL

--- src/components/pan-listener.tsx
+++ src/components/pan-listener.tsx
@@ -1,10 +1,12 @@
 import { debounce } from 'lodash-es';
 import { useCallback, useContext, useEffect } from 'react';
 import { useMap, useMapEvents } from 'react-leaflet';
+import { useNavigate } from 'react-router-dom';
 import { MapContext } from '../service/map.provider';
 
 export const PanListener = () => {
   const map = useMap();
+  const navigate = useNavigate();
   const { searchSpots } = useContext(MapContext) || { searchSpots: () => {} };
 
   useEffect(() => {
@@ -14,7 +16,7 @@ export const PanListener = () => {
   const debouncePan = useCallback(
     debounce(() => {
       const center = map.getCenter();
-      window.history.replaceState(null, 'New Page Title', `/${center.lat.toPrecision(7)}/${center.lng.toPrecision(7)}`);
+      navigate(`/${center.lat.toPrecision(7)}/${center.lng.toPrecision(7)}`);
       searchSpots(map.getBounds());
     }, 500),
     []
